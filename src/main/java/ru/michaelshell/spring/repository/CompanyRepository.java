1d741f08b1b763a4d21e4f61cca7d0b926c44a99
Shel2016 <bvg2003@bk.ru>
2022-10-07 15:18:49 +0300

    xml config completed, annotations start

---
+++ src/main/java/ru/michaelshell/spring/repository/CompanyRepository.java
@@ -0,0 +1,11 @@
+package ru.michaelshell.spring.dao;
+
+import ru.michaelshell.spring.database.pool.ConnectionPool;
+
+public class CompanyRepository {
+    private final ConnectionPool connectionPool;
+
+    public CompanyRepository(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
+}
