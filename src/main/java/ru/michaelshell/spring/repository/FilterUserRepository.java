7f3462ccf3f09a6d477bbef115f3f6ac0d7f8e98
Shel2016 <bvg2003@bk.ru>
2022-10-30 02:31:33 +0300

    jdbc starter

--- src/main/java/ru/michaelshell/spring/repository/FilterUserRepository.java
+++ src/main/java/ru/michaelshell/spring/repository/FilterUserRepository.java
@@ -1,6 +1,8 @@
 package ru.michaelshell.spring.repository;
 
+import ru.michaelshell.spring.database.entity.Role;
 import ru.michaelshell.spring.database.entity.User;
+import ru.michaelshell.spring.dto.PersonalInfo;
 import ru.michaelshell.spring.dto.UserFilter;
 
 import java.util.List;
@@ -8,4 +10,6 @@ import java.util.List;
 public interface FilterUserRepository {
 
     List<User> findAllByFilter(UserFilter userFilter);
+
+    List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
 }
--- src/main/java/ru/michaelshell/spring/repository/FilterUserRepositoryImpl.java
+++ src/main/java/ru/michaelshell/spring/repository/FilterUserRepositoryImpl.java
@@ -2,8 +2,11 @@ package ru.michaelshell.spring.repository;
 
 import com.querydsl.jpa.impl.JPAQuery;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.michaelshell.spring.database.entity.Role;
 import ru.michaelshell.spring.database.entity.User;
 import ru.michaelshell.spring.database.querydsl.QPredicate;
+import ru.michaelshell.spring.dto.PersonalInfo;
 import ru.michaelshell.spring.dto.UserFilter;
 
 import javax.persistence.EntityManager;
@@ -14,7 +17,26 @@ import static ru.michaelshell.spring.database.entity.QUser.user;
 @RequiredArgsConstructor
 public class FilterUserRepositoryImpl implements FilterUserRepository {
 
+    private final String SQL_FIND_BY_COMPANY_ID_AND_ROLE = """
+            SELECT 
+                firstname,
+                lastname,
+                birth_date 
+            FROM users
+            WHERE company_id = ? AND role = ?
+            """;
+
     private final EntityManager entityManager;
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role) {
+        return jdbcTemplate.query(SQL_FIND_BY_COMPANY_ID_AND_ROLE, (rs, rowNum) -> new PersonalInfo(
+                rs.getString("firstname"),
+                rs.getString("lastname"),
+                rs.getDate("birth_date").toLocalDate()
+        ), companyId, role.name());
+    }
 
     @Override
     public List<User> findAllByFilter(UserFilter filter) {
@@ -49,4 +71,6 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
 //
 //        return entityManager.createQuery(criteria).getResultList();
     }
+
+
 }
