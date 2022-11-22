53b5c1e0308422aa39cffea2b686e661f3e7a3a5
Shel2016 <bvg2003@bk.ru>
2022-11-23 01:47:36 +0300

    REST practice

---
+++ src/main/java/ru/michaelshell/spring/http/rest/UserRestController.java
@@ -0,0 +1,64 @@
+package ru.michaelshell.spring.http.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.michaelshell.spring.dto.PageResponse;
+import ru.michaelshell.spring.dto.UserCreateEditDto;
+import ru.michaelshell.spring.dto.UserFilter;
+import ru.michaelshell.spring.dto.UserReadDto;
+import ru.michaelshell.spring.service.CompanyService;
+import ru.michaelshell.spring.service.UserService;
+import ru.michaelshell.spring.validation.group.CreateAction;
+import ru.michaelshell.spring.validation.group.UpdateAction;
+
+import javax.validation.groups.Default;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final UserService userService;
+    private final CompanyService companyService;
+
+
+    @GetMapping
+    public PageResponse<UserReadDto> findAll(UserFilter filter, Pageable pageable) {
+        Page<UserReadDto> page = userService.findAll(filter, pageable);
+        return PageResponse.of(page);
+    }
+
+    @GetMapping("/{id}")
+    public UserReadDto findById(@PathVariable Long id) {
+        return userService.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserReadDto create(@Validated({Default.class, CreateAction.class}) @RequestBody UserCreateEditDto user) {
+        return userService.create(user);
+    }
+
+    @PutMapping("/{id}")
+    public UserReadDto update(@PathVariable Long id,
+                         @Validated({Default.class, UpdateAction.class}) @RequestBody UserCreateEditDto user) {
+        return userService.update(id, user)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+        if (!userService.delete(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
